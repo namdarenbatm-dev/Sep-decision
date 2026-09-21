126d025318a14bbf7f505f1f9b0b52f1a850012e
namdarenbatm-dev <namdar.enbatm@gmail.com>
2026-09-21 10:17:22 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='sep-dss-pwa-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>{
+    if(cached) return cached;
+    return fetch(e.request).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match('./index.html'));
+  }));
+});
+self.addEventListener('message',e=>{ if(e.data==='SKIP_WAITING') self.skipWaiting(); });
